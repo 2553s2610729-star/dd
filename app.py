869d5116d241d9cb1fd3f0eb8478792d74e2b644
2553s2610729-star <2553s2610729@g.cnees.kr>
2026-05-26 10:17:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
-st.title('배고프다')
-st.write('학교 조퇴해야지')
+import random
+
+st.title("🎲 랜덤 코디 추천")
+
+tops = ["흰 반팔", "검정 후드", "니트", "셔츠"]
+bottoms = ["청바지", "슬랙스", "반바지", "조거팬츠"]
+
+if st.button("코디 추천 받기"):
+    top = random.choice(tops)
+    bottom = random.choice(bottoms)
+
+    st.write(f"상의: {top}")
+    st.write(f"하의: {bottom}")
